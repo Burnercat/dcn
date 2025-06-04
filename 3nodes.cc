b70da0dd4ba5842cef02bf9e34ff93383df30b9b
BurnerCat <p70157015@gmail.com>
2025-06-04 11:31:31 +0530

    Update 3nodes.cc

--- 3nodes.cc
+++ 3nodes.cc
@@ -71,6 +71,11 @@ NS_LOG_COMPONENT_DEFINE("TrafficControlExample");
  * \param oldValue Old velue.
  * \param newValue New value.
  */
+/*void
+TcPacketsInQueueTrace(uint32_t oldValue, uint32_t newValue)
+{
+    std::cout << "TcPacketsInQueue " << oldValue << " to " << newValue << std::endl;
+}
 
 /**
  * Packets in the device queue trace.
@@ -78,20 +83,28 @@ NS_LOG_COMPONENT_DEFINE("TrafficControlExample");
  * \param oldValue Old velue.
  * \param newValue New value.
  */
-
+/*void
+DevicePacketsInQueueTrace(uint32_t oldValue, uint32_t newValue)
+{
+    std::cout << "DevicePacketsInQueue " << oldValue << " to " << newValue << std::endl;
+}
 
 /**
  * TC Soujoun time trace.
  *
  * \param sojournTime The soujourn time.
  */
-
-
+/*void
+SojournTimeTrace(Time sojournTime)
+{
+    std::cout << "Sojourn time " << sojournTime.ToDouble(Time::MS) << "ms" << std::endl;
+}
+*/
 int
 main(int argc, char* argv[])
 {
     double simulationTime = 10; // seconds
-    std::string transportProt = "Tcp";
+    std::string transportProt = "Udp";
     std::string socketType;
 
     CommandLine cmd(__FILE__);
@@ -108,29 +121,51 @@ main(int argc, char* argv[])
     }
 
     NodeContainer nodes;
-    nodes.Create(2);
+    nodes.Create(3);
 
     PointToPointHelper pointToPoint;
     pointToPoint.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
     pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
     pointToPoint.SetQueue("ns3::DropTailQueue", "MaxSize", StringValue("1p"));
 
-    NetDeviceContainer devices;
-    devices = pointToPoint.Install(nodes);
+    NetDeviceContainer devices01;
+    devices01 = pointToPoint.Install(nodes.Get(0),nodes.Get(1));
+    
+    NetDeviceContainer devices12;
+    devices12 = pointToPoint.Install(nodes.Get(1),nodes.Get(2));
 
     InternetStackHelper stack;
     stack.Install(nodes);
+/*
+    TrafficControlHelper tch;
+    tch.SetRootQueueDisc("ns3::RedQueueDisc");
+    QueueDiscContainer qdiscs = tch.Install(devices);
 
+    Ptr<QueueDisc> q = qdiscs.Get(1);
+    q->TraceConnectWithoutContext("PacketsInQueue", MakeCallback(&TcPacketsInQueueTrace));
+    Config::ConnectWithoutContext(
+        "/NodeList/1/$ns3::TrafficControlLayer/RootQueueDiscList/0/SojournTime",
+        MakeCallback(&SojournTimeTrace));
+
+    Ptr<NetDevice> nd = devices.Get(1);
+    Ptr<PointToPointNetDevice> ptpnd = DynamicCast<PointToPointNetDevice>(nd);
+    Ptr<Queue<Packet>> queue = ptpnd->GetQueue();
+    queue->TraceConnectWithoutContext("PacketsInQueue", MakeCallback(&DevicePacketsInQueueTrace));
+*/
     Ipv4AddressHelper address;
+    
     address.SetBase("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer interfaces01 = address.Assign(devices01);
+    
+    address.SetBase("10.1.2.0","255.255.255.0");
+    Ipv4InterfaceContainer interfaces12 = address.Assign(devices12);
 
-    Ipv4InterfaceContainer interfaces = address.Assign(devices);
-
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
     // Flow
     uint16_t port = 7;
     Address localAddress(InetSocketAddress(Ipv4Address::GetAny(), port));
     PacketSinkHelper packetSinkHelper(socketType, localAddress);
-    ApplicationContainer sinkApp = packetSinkHelper.Install(nodes.Get(0));
+    ApplicationContainer sinkApp = packetSinkHelper.Install(nodes.Get(2));
 
     sinkApp.Start(Seconds(0.0));
     sinkApp.Stop(Seconds(simulationTime + 0.1));
@@ -145,11 +180,11 @@ main(int argc, char* argv[])
     onoff.SetAttribute("DataRate", StringValue("50Mbps")); // bit/s
     ApplicationContainer apps;
 
-    InetSocketAddress rmt(interfaces.GetAddress(0), port);
+    InetSocketAddress rmt(interfaces12.GetAddress(1), port);
     rmt.SetTos(0xb8);
     AddressValue remoteAddress(rmt);
     onoff.SetAttribute("Remote", remoteAddress);
-    apps.Add(onoff.Install(nodes.Get(1)));
+    apps.Add(onoff.Install(nodes.Get(0)));
     apps.Start(Seconds(1.0));
     apps.Stop(Seconds(simulationTime + 0.1));
 
@@ -181,9 +216,42 @@ main(int argc, char* argv[])
     }
     std::cout << "  Packets/Bytes Dropped by Queue Disc:   " << packetsDroppedByQueueDisc << " / "
               << bytesDroppedByQueueDisc << std::endl;
-  
+    /*uint32_t packetsDroppedByNetDevice = 0;
+    uint64_t bytesDroppedByNetDevice = 0;
+    if (stats[1].packetsDropped.size() > Ipv4FlowProbe::DROP_QUEUE)
+    {
+        packetsDroppedByNetDevice = stats[1].packetsDropped[Ipv4FlowProbe::DROP_QUEUE];
+        bytesDroppedByNetDevice = stats[1].bytesDropped[Ipv4FlowProbe::DROP_QUEUE];
+    }
+    std::cout << "  Packets/Bytes Dropped by NetDevice:   " << packetsDroppedByNetDevice << " / "
+              << bytesDroppedByNetDevice << std::endl;
+    std::cout << "  Throughput: "
+              << stats[1].rxBytes * 8.0 /
+                     (stats[1].timeLastRxPacket.GetSeconds() -
+                      stats[1].timeFirstRxPacket.GetSeconds()) /
+                     1000000
+              << " Mbps" << std::endl;
+    std::cout << "  Mean delay:   " << stats[1].delaySum.GetSeconds() / stats[1].rxPackets
+              << std::endl;
+    std::cout << "  Mean jitter:   " << stats[1].jitterSum.GetSeconds() / (stats[1].rxPackets - 1)
+              << std::endl;
+    auto dscpVec = classifier->GetDscpCounts(1);
+    for (auto p : dscpVec)
+    {
+        std::cout << "  DSCP value:   0x" << std::hex << static_cast<uint32_t>(p.first) << std::dec
+                  << "  count:   " << p.second << std::endl;
+    }
+*/
     Simulator::Destroy();
 
-
+   /* std::cout << std::endl << "*** Application statistics ***" << std::endl;
+    double thr = 0;
+    uint64_t totalPacketsThr = DynamicCast<PacketSink>(sinkApp.Get(0))->GetTotalRx();
+    thr = totalPacketsThr * 8 / (simulationTime * 1000000.0); // Mbit/s
+    std::cout << "  Rx Bytes: " << totalPacketsThr << std::endl;
+    std::cout << "  Average Goodput: " << thr << " Mbit/s" << std::endl;
+    std::cout << std::endl << "*** TC Layer statistics ***" << std::endl;
+    std::cout << q->GetStats() << std::endl;*/
     return 0;
 }
+
