08e97babec71fe726979bab303b81b67bfee89d8
BurnerCat <p70157015@gmail.com>
2025-06-26 18:08:29 +0530

    Add files via upload

---
+++ TDM.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class TDM{
+public static void main(String args[]){
+	int n,i,qt,count= 0,temp,sq=0,bt[],wt[],tat[],rem_bt[];
+	float awt=0,atat=0;
+	bt = new int[10];
+	wt = new int[10];
+	tat = new int[10];
+	rem_bt = new int[10];
+	Scanner s = new Scanner(System.in);
+	System.out.print("Enter no of stations");
+	n = s.nextInt();
+	System.out.println("Entr proceessing times");
+	for (i = 0;i<n;i++){
+		System.out.print("S"+i+"=");
+		bt[i]=s.nextInt();
+		rem_bt[i] = bt[i];
+	}
+	System.out.print("Enter frame size");
+	qt = s.nextInt();
+	while(true)
+	{
+		for(i=0,count=0;i<n;i++){
+		temp = qt;
+		if(rem_bt[i] == 0){
+		count++;
+		continue;
+		}
+		if(rem_bt[i]>qt)
+			rem_bt[i]=rem_bt[i]-qt;
+		else
+		if(rem_bt[i]>=0){
+		temp = rem_bt[i];
+		rem_bt[i]=0;
+		}
+		sq = sq+temp;
+		tat[i] = sq;
+		}
+		if(n== count)
+		break;
+	}
+	System.out.print("--------------------------------------------------------------------------------");
+	System.out.print("\nStation\t Processing Time\t Completion Time\t Waiting Time\n");
+	System.out.print("--------------------------------------------------------------------------------");
+	for(i=0;i<n;i++){
+		wt[i]=tat[i]-bt[i];
+		awt=awt+wt[i];
+		atat=atat+tat[i];
+		System.out.print("\n \t"+(i+1)+"\t \t"+bt[i]+"\t\t "+tat[i]+"\t\t\t "+wt[i]+"\n");
+	
+}
+}
+}
